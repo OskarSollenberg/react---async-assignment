b12017fcc99e2ed95a00a7df4457a9cff0b7f9ac
Bwunkaz <122973984+Bwunkaz@users.noreply.github.com>
2023-12-19 09:38:08 +0100

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,3 @@
-import Header from "./Header.jsx";
-import Footer from "./Footer.jsx";
-import Food from "./Food.jsx";
-
-function App() {
-    return (
-        <>
-            <Header />
-            <Food />
-            <Footer />
-        </>
-    );
-}
+function App() {}
 
 export default App;
---
+++ src/Student.jsx
@@ -0,0 +1,7 @@
+function student(props) {
+    <div>
+        <p Name: {props}></p>
+    </div>
+}
+
+export default student;
