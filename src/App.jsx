a97bfe8069f95c811229e8c9ef3b630afd044592
Bwunkaz <122973984+Bwunkaz@users.noreply.github.com>
2023-12-20 17:44:50 +0100

    Done with props

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,14 @@
-function App() {}
+import Student from "./Student.jsx";
+function App() {
+    return (
+        <div>
+            <Student name="Spongebob" age={30} isStudent={true} />
+            <Student name="Patrick" age={42} isStudent={false} />
+            <Student name="Sqidward" age={50} isStudent={false} />
+            <Student name="Sandy" age={27} isStudent={true} />
+            <Student name="Larry" />
+        </div>
+    );
+}
 
 export default App;
